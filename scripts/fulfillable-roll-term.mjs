dcc95e10a2a0199111f88d5f0f367bf7811f85c8
Andrew (Atropos) <admin@foundryvtt.com>
2023-06-11 17:34:05 -0400

    Fix broken JS import paths. Convert all files to mjs. Convert to 2 spaces

---
+++ scripts/fulfillable-roll-term.mjs
@@ -0,0 +1,123 @@
+export default class FulfillableRollTerm extends DiceTerm {
+
+  MODIFIERS = {};
+
+  /* -------------------------------------------- */
+
+  evaluate({minimize=false, maximize=false, async=false, fulfilled=null}={}) {
+    if ( this._evaluated ) {
+      throw new Error(`The ${this.constructor.name} has already been evaluated and is now immutable`);
+    }
+    this._evaluated = true;
+    return async ? this._evaluate({minimize, maximize, fulfilled}) : this._evaluateSync({minimize, maximize});
+  }
+
+  /* -------------------------------------------- */
+
+  async _evaluate({minimize=false, maximize=false, fulfilled=null}={}) {
+    if ( (this.number > 999) ) {
+      throw new Error(`You may not evaluate a DiceTerm with more than 999 requested results`);
+    }
+    for ( let n=1; n <= this.number; n++ ) {
+      await this.roll({minimize, maximize, fulfilled, n});
+    }
+    this._evaluateModifiers();
+    return this;
+  }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  async roll({minimize=false, maximize=false, fulfilled, n}={}) {
+    const roll = {result: undefined, active: true};
+    if ( minimize ) roll.result = Math.min(1, this.faces);
+    else if ( maximize ) roll.result = this.faces;
+    else {
+      // Grab the result from fulfilled
+      const id = `d${this.faces}-${n}`;
+      if ( fulfilled && fulfilled.has(id) ) {
+        const result = fulfilled.get(id);
+        roll.result = Number.parseInt(result);
+      }
+      else {
+        roll.result = await this._fulfillRoll();
+      }
+    }
+    this.results.push(roll);
+    return roll;
+  }
+
+  /* -------------------------------------------- */
+
+  async _fulfillRoll() {
+    // This term was not already fulfilled, so use the original FVTT roller
+    return Math.ceil(CONFIG.Dice.randomUniform() * this.faces);
+  }
+
+  /* -------------------------------------------- */
+
+  static _fromData(data) {
+    const term = new this(data);
+    term._evaluated = data.evaluated ?? true;
+    term._fromData = true;
+    return term;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Sequentially evaluate each dice roll modifier by passing the term to its evaluation function
+   * Augment or modify the results array.
+   * @private
+   */
+  _evaluateModifiers() {
+    const requested = foundry.utils.deepClone(this.modifiers);
+    this.modifiers = [];
+
+    // Iterate over requested modifiers
+    for ( let m of requested ) {
+      let command = m.match(/[A-z]+/)[0].toLowerCase();
+
+      // Matched command
+      if ( command in this.MODIFIERS ) {
+        this._evaluateModifier(command, m);
+        continue;
+      }
+
+      // Unmatched compound command
+      // Sort modifiers from longest to shortest to ensure that the matching algorithm greedily matches the longest
+      // prefixes first.
+      const modifiers = Object.keys(this.MODIFIERS).sort((a, b) => b.length - a.length);
+      while ( !!command ) {
+        let matched = false;
+        for ( let cmd of modifiers ) {
+          if ( command.startsWith(cmd) ) {
+            matched = true;
+            this._evaluateModifier(cmd, cmd);
+            command = command.replace(cmd, "");
+            break;
+          }
+        }
+        if ( !matched ) command = "";
+      }
+    }
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Evaluate a single modifier command, recording it in the array of evaluated modifiers
+   * @param {string} command        The parsed modifier command
+   * @param {string} modifier       The full modifier request
+   * @private
+   */
+  _evaluateModifier(command, modifier) {
+    let fn = this.MODIFIERS[command];
+    if ( typeof fn === "string" ) fn = this[fn];
+    if ( fn instanceof Function ) {
+      const result = fn.call(this, modifier);
+      const earlyReturn = (result === false) || (result === this); // handling this is backwards compatibility
+      if ( !earlyReturn ) this.modifiers.push(modifier.toLowerCase());
+    }
+  }
+}
--- scripts/unfulfilled-rolls.mjs
+++ scripts/unfulfilled-rolls.mjs
@@ -1,63 +1,61 @@
-import {FulfillableRoll} from "./FulfillableRoll.mjs";
-import DiceConfig from "./apps/diceConfiguration/dice-config.mjs";
+import FulfillableRoll from "./fulfillable-roll.mjs";
+import DiceConfig from "./apps/dice-config.mjs";
 
 Hooks.once('init', async function() {
 
-    game.settings.register("unfulfilled-rolls", "diceSettings", {
-        config: false,
-        default: {},
-        type: Object,
-        scope: "client"
-    });
+  game.settings.register("unfulfilled-rolls", "diceSettings", {
+    config: false,
+    default: {},
+    type: Object,
+    scope: "client"
+  });
 
-    //CONFIG.Dice.FulfillableRoll = FulfillableRoll;
-    CONFIG.Dice.DieTypes = [
-        { id: "d4", faces: 4, icon: "fa-dice-d4" },
-        { id: "d6", faces: 6, icon: "fa-dice-d6" },
-        { id: "d8", faces: 8, icon: "fa-dice-d8" },
-        { id: "d10", faces: 10, icon: "fa-dice-d10" },
-        { id: "d12", faces: 12, icon: "fa-dice-d12" },
-        { id: "d20", faces: 20, icon: "fa-dice-d20" },
-        { id: "d100", faces: 100, icon: "fa-percent" }
-    ];
-    CONFIG.Dice.FulfillmentMethods = {
-        "fvtt": "Foundry VTT Digital Roll",
-        "input": "Manual Input",
-        "bluetooth": "Bluetooth Dice",
-    };
+  CONFIG.Dice.DieTypes = [
+    { id: "d4", faces: 4, icon: "fa-dice-d4" },
+    { id: "d6", faces: 6, icon: "fa-dice-d6" },
+    { id: "d8", faces: 8, icon: "fa-dice-d8" },
+    { id: "d10", faces: 10, icon: "fa-dice-d10" },
+    { id: "d12", faces: 12, icon: "fa-dice-d12" },
+    { id: "d20", faces: 20, icon: "fa-dice-d20" },
+    { id: "d100", faces: 100, icon: "fa-percent" }
+  ];
+  CONFIG.Dice.FulfillmentMethods = {
+    "fvtt": "Foundry VTT Digital Roll",
+    "input": "Manual Input",
+    "bluetooth": "Bluetooth Dice",
+  };
 
-    let providers = {
-        "none": {
-            label: "None",
-        },
-    };
-    Hooks.callAll("unfulfilled-rolls-bluetooth", providers);
-    CONFIG.Dice.BluetoothDieProviders = providers;
+  let providers = {
+    "none": {
+      label: "None",
+    },
+  };
+  Hooks.callAll("unfulfilled-rolls-bluetooth", providers);
+  CONFIG.Dice.BluetoothDieProviders = providers;
 
-    // Replace the `Roll` in the global namespace with `FulfillableRoll` class
-    Roll = FulfillableRoll;
-    CONFIG.Dice.rolls.unshift(FulfillableRoll);
-    //CONFIG.Dice.termTypes.DiceTerm = FulfillableRollTerm;
+  // Replace the `Roll` in the global namespace with `FulfillableRoll` class
+  Roll = FulfillableRoll;
+  CONFIG.Dice.rolls.unshift(FulfillableRoll);
 
-    if ( game.system.id === "dnd5e" ) {
-        // Monkey patch the evaluate methods of the D20Roll and DamageRoll classes
-        CONFIG.Dice.D20Roll.prototype.evaluate = FulfillableRoll.prototype.evaluate;
-        CONFIG.Dice.D20Roll.prototype._evaluate = FulfillableRoll.prototype._evaluate;
-        CONFIG.Dice.D20Roll.prototype._evaluateSync = FulfillableRoll.prototype._evaluateSync;
-        CONFIG.Dice.DamageRoll.prototype.evaluate = FulfillableRoll.prototype.evaluate;
-        CONFIG.Dice.DamageRoll.prototype._evaluate = FulfillableRoll.prototype._evaluate;
-        CONFIG.Dice.DamageRoll.prototype._evaluateSync = FulfillableRoll.prototype._evaluateSync;
-    }
+  if ( game.system.id === "dnd5e" ) {
+    // Monkey patch the evaluate methods of the D20Roll and DamageRoll classes
+    CONFIG.Dice.D20Roll.prototype.evaluate = FulfillableRoll.prototype.evaluate;
+    CONFIG.Dice.D20Roll.prototype._evaluate = FulfillableRoll.prototype._evaluate;
+    CONFIG.Dice.D20Roll.prototype._evaluateSync = FulfillableRoll.prototype._evaluateSync;
+    CONFIG.Dice.DamageRoll.prototype.evaluate = FulfillableRoll.prototype.evaluate;
+    CONFIG.Dice.DamageRoll.prototype._evaluate = FulfillableRoll.prototype._evaluate;
+    CONFIG.Dice.DamageRoll.prototype._evaluateSync = FulfillableRoll.prototype._evaluateSync;
+  }
 });
 
 Hooks.on("renderSettings", (app, html, data) => {
-    const diceConfigButton = document.createElement("button");
-    diceConfigButton.type = "button";
-    diceConfigButton.classList.add("open-dice-config");
-    diceConfigButton.innerHTML = `<i class="fas fa-dice-d20"></i> Open Dice Configuration`;
-    diceConfigButton.addEventListener("click", () => {
-        const diceConfig = new DiceConfig();
-        diceConfig.render(true);
-    });
-    html.find("button[data-action='controls']").after(diceConfigButton);
+  const diceConfigButton = document.createElement("button");
+  diceConfigButton.type = "button";
+  diceConfigButton.classList.add("open-dice-config");
+  diceConfigButton.innerHTML = `<i class="fas fa-dice-d20"></i> Open Dice Configuration`;
+  diceConfigButton.addEventListener("click", () => {
+    const diceConfig = new DiceConfig();
+    diceConfig.render(true);
+  });
+  html.find("button[data-action='controls']").after(diceConfigButton);
 });
